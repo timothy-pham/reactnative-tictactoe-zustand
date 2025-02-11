49cf192c8d85ad5d694eab479ba728ea6643e0d8
Dat Pham <phamtiendat.dev@gmail.com>
2025-02-11 15:06:35 +0700

    simple tic tac toe game

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
+const { singleQuote } = require("./.prettierrc");
+
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'curly': 'off',
+    'no-unused-vars': 'off',
+    singleQuote: singleQuote,
+  },
 };
